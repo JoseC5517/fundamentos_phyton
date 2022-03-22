5922fd03d0598c65619e1ad8ae24700f170a312a
José C. C <80420702+JoseC5517@users.noreply.github.com>
2022-03-22 16:03:33 +0000

    ejercicio2

---
+++ ejercicios/ejercicio2.py
@@ -0,0 +1,7 @@
+#Usuario debe digitar su nombre y edad, imprimir que tipo de dato son las variables y decir en que a;o nacio la persona
+
+nombre=input("Digite su nombre: ")
+edad=int(input("Digite su edad: "))
+
+         
+
