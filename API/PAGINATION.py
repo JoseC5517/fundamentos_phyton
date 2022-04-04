f7b30ec7e44462f67970b163cf4fd252a1bf6205
José C. C <80420702+JoseC5517@users.noreply.github.com>
2022-04-04 14:38:52 +0000

    .

--- API/PAGINATION.py
+++ API/PAGINATION.py
@@ -1,24 +1,20 @@
 import requests
-
-url = f"https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
-respuesta = requests.get(url)
-datos=respuesta.json()
-nombres=[]
-
-for x in range(len(datos['results'])):
-  nombres.append(datos['results'][x]['name'])
-  
-for i,x in enumerate(nombres,1):
-  print(f"{i}. {x}")
-
-pag = input("Desea ver la siguiente pagina (y/n): ").lower().replace(" ","")
-if pag == "y":
-  url = f"https://pokeapi.co/api/v2/pokemon?limit=20&offset=21"
+x1 = 0
+x2 = int(input("Cuantos resutados quieres ver por pagina: "))
+while x1 <= 898:
+  url = f"https://pokeapi.co/api/v2/pokemon?limit={x2}&offset={x1}"
   respuesta = requests.get(url)
   datos=respuesta.json()
   nombres=[]
   for x in range(len(datos['results'])):
     nombres.append(datos['results'][x]['name'])
   
-  for i,x in enumerate(nombres,21):
-    print(f"{i}. {x}")
+  for i,x in enumerate(nombres,1):
+    print(f"{i+x1}. {x}")
+
+  pag = input("Desea ver la siguiente pagina (y/n): ").lower().replace(" ","")
+  
+  if pag == "n":
+    break
+  else:
+    x1 = x1 + x2
