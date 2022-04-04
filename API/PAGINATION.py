ae414bb68606c861b257123a119dfe2554eebb93
José C. C <80420702+JoseC5517@users.noreply.github.com>
2022-04-04 15:20:43 +0000

    .

--- API/PAGINATION.py
+++ API/PAGINATION.py
@@ -1,7 +1,7 @@
 import requests
 x1 = 0
 x2 = int(input("Cuantos resutados quieres ver por pagina: "))
-while x1 <= 898:
+while x1 in range(899):
   url = f"https://pokeapi.co/api/v2/pokemon?limit={x2}&offset={x1}"
   respuesta = requests.get(url)
   datos=respuesta.json()
@@ -12,9 +12,12 @@ while x1 <= 898:
   for i,x in enumerate(nombres,1):
     print(f"{i+x1}. {x}")
 
-  pag = input("Desea ver la siguiente pagina (y/n): ").lower().replace(" ","")
-  
-  if pag == "n":
-    break
-  else:
+  pag = input("Desea ver la siguiente pagina o a la anterior (a/s): ").lower().replace(" ","")
+
+  if pag == "a":
+    x1 = x1 - x2
+  elif pag == "s":
     x1 = x1 + x2
+  else:
+    print("ERROR: Dato no valido. ")
+    break
