4081422dab84ec81065e988da8ec846a0a3e0e3c
José C. C <80420702+JoseC5517@users.noreply.github.com>
2022-04-04 01:48:13 +0000

    APIPOKEMON

--- API/apipoke.py
+++ API/apipoke.py
@@ -1,19 +1,44 @@
 import requests
-usuario = int(input("Digite el ID del pokemon: "))
+
+url1 = f"https://pokeapi.co/api/v2/pokemon?limit=100&offset=200"
+respuesta1 = requests.get(url1)
+dt=respuesta1.json()
+
+while 1 == 1:
+  usuario = int(input("Digite el ID del pokemon: "))
+  if usuario in range(1,dt["count"]+1):
+    break
+    
 url = f"https://pokeapi.co/api/v2/pokemon/{usuario}"
 respuesta = requests.get(url)
+
 if respuesta:
   datos=respuesta.json()
   habilidades = []
-  for habilidad in habilidades:
-    habilidades.append(datos['abilities']['ability']['name'])
-print(f"""NOMBRE: {datos['name']}
-      PESO: {int(datos['weight'])/10}
-      ALTURA: {datos['height']}
-      CANTIDAD DE HABILIDADES: {len(datos['abilities'])}
-      HABILIDADES:""")
+  tipos = []
+  
+  for habilidad in range(len(datos['abilities'])):
+    habilidades.append(datos['abilities'][habilidad]['ability']['name'])
+    
+print(f"""
+NOMBRE: {datos['name']}\n
+PESO: {int(datos['weight'])/10}\n
+ALTURA: {datos['height']}\n
+CANTIDAD DE HABILIDADES: {len(datos['abilities'])}\n
+HABILIDADES:""")  
+
 for i,x in enumerate(habilidades,1):
-  print(f"{i}. {idioma}")
+  print(f"{i}. {x}")
+  
+print(f"""
+CANTIDAD DE MOVIMIENTOS: {len(datos['moves'])}\n
+TIPOS: """)
+
+for tipo in range(len(datos['types'])):
+  tipos.append(datos['types'][tipo]['type']['name'])
+  
+for i,x in enumerate(tipos,1):
+  print(f"{i}. {x}")
   
 #   idiomas=[]
 #   habilidades=[]
