a3c4f2a31b494c10b8cc9358240a1f41dc44da53
José C. C <80420702+JoseC5517@users.noreply.github.com>
2022-04-08 15:04:51 +0000

    .

--- API/funciones.py
+++ API/funciones.py
@@ -1,4 +1,25 @@
 import requests
+import re
+
+def obtenerPrimerUltimo(lista):
+  primero = None 
+  ultimo = None 
+  for pokemon in lista:
+    id_pokemon = pokemon['url'].split('/')[6]
+    if primero == None:
+      primero = int(id_pokemon)
+      
+  ultimo = int(id_pokemon)
+  return (primero, ultimo)
+
+def obtenerDatos(url):
+  respuesta = requests.get(url)
+
+  if respuesta:
+    return respuesta.json()
+  else:
+    raise Exception("No hay datos")
+  
 def verListado(url):
   print("Listado de pokemones")
   
@@ -6,19 +27,26 @@ def verListado(url):
   if respuesta:
     datos = respuesta.json()
 
-    primero = None 
-    ultimo = None 
+
   
   for pokemon in datos['results']:
     id_pokemon = pokemon['url'].split('/')[6]
     nombre= pokemon['name']
-    if primero == None:
-      primero = int(id_pokemon)
+  #   if primero == None:
+  #     primero = int(id_pokemon)
       
     print(id_pokemon, nombre)
 
-  ultimo = int(id_pokemon)
-  
+  # ultimo = int(id_pokemon)
+def habilidadesPoke(datos):
+  habilidades = []
+  print("\n\nhabilidades:\n")
+  print ("{:<5} {:<40} ".format('ID','Nombre'))
+  print("-"*20)
+  for habilidad in range(len(datos['abilities'])):
+    id = datos['abilities'][habilidad]['ability']['url'].split('/')[6]
+    nombre = datos['abilities'][habilidad]['ability']['name']
+    print("{:<5} {:<40} ".format(id,nombre))
 def verPokemon():
   print("ver pokemon")
 def buscarPokemonPorId():
--- API/reto_poke.py
+++ API/reto_poke.py
@@ -14,47 +14,23 @@ while not re.search(REGEX_NUMBERS, limite):
 url = BASE_URL+f"pokemon?limit={limite}&offset=0"
 
 verListado(url)
+listadoPokemones= obtenerDatos(url)
+primeroUltimo = obtenerPrimerUltimo(listadoPokemones['results'])
 
-respuesta = requests.get(url)
+print(primeroUltimo)
 
-if respuesta:
-  datos = respuesta.json()
-
-  primero = None 
-  ultimo = None 
-  
-  for pokemon in datos['results']:
-    id_pokemon = pokemon['url'].split('/')[6]
-    nombre= pokemon['name']
-    if primero == None:
-      primero = int(id_pokemon)
-      
-    print(id_pokemon, nombre)
-
-  ultimo = int(id_pokemon)
-
+primero = primeroUltimo[0]
+ultimo = primeroUltimo[1]
+id_digitado = input(f"Digite un id [{primero}-{ultimo}]:")
+resultado = re.search(REGEX_NUMBERS, id_digitado)
+while(not re.search(REGEX_NUMBERS, id_digitado)) or (int(id_digitado) < primero or int(id_digitado) > ultimo):
   id_digitado = input(f"Digite un id [{primero}-{ultimo}]:")
-  resultado = re.search(REGEX_NUMBERS, id_digitado)
-  while(not re.search(REGEX_NUMBERS, id_digitado)) or (int(id_digitado) < primero or int(id_digitado) > ultimo):
-    id_digitado = input(f"Digite un id [{primero}-{ultimo}]:")
-  
-  url1 = BASE_URL + f"pokemon/{id_digitado}"
-  respuesta11 = requests.get(url1)
-  if respuesta11:
-    datos=respuesta11.json()
-  print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format('ID','Nombre','Altura','# de Habilidad', '# de Movimientos\n'))
-  print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format(id_digitado,datos['name'],int(datos['weight'])/10,datos['height']/10, len(datos['abilities'])))
 
-  habilidades = []
-  tipos = []
-  id1 = None
-  print("\n\nhabilidades:\n")
-  print ("{:<5} {:<40} ".format('ID','Nombre'))
-  print("-"*20)
-  for habilidad in range(len(datos['abilities'])):
-    id = datos['abilities'][habilidad]['ability']['url'].split('/')[6]
-    nombre = datos['abilities'][habilidad]['ability']['name']
-    print(f"{id}    {nombre}")
+url1 = BASE_URL + f"pokemon/{id_digitado}"
+respuesta11 = requests.get(url1)
+if respuesta11:
+  datos=respuesta11.json()
+print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format('ID','Nombre','Altura','# de Habilidad', '# de Movimientos\n'))
+print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format(id_digitado,datos['name'],int(datos['weight'])/10,datos['height']/10, len(datos['abilities'])))
+habilidadesPoke(datos)
 
-else: 
-  print(f"Ocurrio un error ({respuesta.status_code}) al obtener los datos")
