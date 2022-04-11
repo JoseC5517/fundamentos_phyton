3d7282278d021d63d9f9934c2db7417418d24783
José C. C <80420702+JoseC5517@users.noreply.github.com>
2022-04-11 21:53:45 +0000

    .

--- API/funciones.py
+++ API/funciones.py
@@ -71,22 +71,31 @@ def verPokemonnom(BASE_URL,nom_digitado):
   if respuesta11:
     datos=respuesta11.json()
   print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format('ID','Nombre','Altura','# de Habilidad', '# de Movimientos\n'))
-  print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format(id_digitado,datos['name'],int(datos['weight'])/10,datos['height']/10, len(datos['abilities'])))
+  print ("{:<5} {:<15} {:<10} {:<20} {:<10}".format(datos['id'],datos['name'],int(datos['weight'])/10,datos['height']/10, len(datos['abilities'])))
   return datos
   
 def buscarPokemonPorId(primero,ultimo,REGEX_NUMBERS):
   id_digitado = input(f"Digite un id [{primero}-{ultimo}]:")
   resultado = re.search(REGEX_NUMBERS, id_digitado)
+  
   while(not re.search(REGEX_NUMBERS, id_digitado)) or (int(id_digitado) < primero or int(id_digitado) > ultimo):
     id_digitado = input(f"Digite un id [{primero}-{ultimo}]:")
   return id_digitado
   
-def buscarPokemonPorNombre(REGEX_NOMBRES):
-  nom_digitado = input(f"Digite un nombre:")
+def buscarPokemonPorNombre(REGEX_NOMBRES,url):
+  nom_digitado = input(f"Digite un nombre: ")
   resultado = re.search(REGEX_NOMBRES, nom_digitado)
+  respuesta11 = requests.get(url)
+  l = []
+  if respuesta11:
+    datos=respuesta11.json()
+    for x in range(len(datos['results'])):
+      l.append(datos['results'][x]['name'])
   while(not re.search(REGEX_NOMBRES, nom_digitado)):
-    nom_digitado = input(f"Digite un nombre:") 
-    
+    nom_digitado = input(f"Digite un nombre: ") 
+  while not nom_digitado in l:
+    nom_digitado = input(f"Digite un nombre valido: ") 
+
   return nom_digitado
 def salir():
   print("bye")
--- API/reto_poke.py
+++ API/reto_poke.py
@@ -12,25 +12,55 @@ limite=input("Digite la cantidad a mostrar de pokemones: ")
 while not re.search(REGEX_NUMBERS, limite):
   limite=input("Digite la cantidad a mostrar de pokemones: ")
 
-url = BASE_URL+f"pokemon?limit={limite}&offset=0"
+x2 = 0
+  
+while x2 in range(899):
+  limite = int(limite)
+  url = BASE_URL+f"pokemon?limit={limite}&offset={x2}"
+  
+  verListado(url)
+  listadoPokemones= obtenerDatos(url)
+  primeroUltimo = obtenerPrimerUltimo(listadoPokemones['results'])
+  
+  print(primeroUltimo)
+  
+  primero = primeroUltimo[0]
+  ultimo = primeroUltimo[1]
+  
+  
+  conf = input("Desea buscar el pokemon por nombre y/n: ")
+  
+  while True:
+      patron = "^[yn]{1}$"
+      resultado22 = re.search(patron, conf)
+      if resultado22:
+          break
+      conf = input("Desea buscar el pokemon por nombre y/n: ")
+  
+  if conf == "y":
+    nom_digitado = buscarPokemonPorNombre(REGEX_NOMBRES,url)
+    datos1 = verPokemonnom(BASE_URL,nom_digitado) 
+    
+  else:
+    id_digitado = buscarPokemonPorId(primero,ultimo,REGEX_NUMBERS)
+    datos1 = verPokemonID(BASE_URL,id_digitado)
+  habilidadesPoke(datos1)
+  tiposPoke(datos1)
 
-verListado(url)
-listadoPokemones= obtenerDatos(url)
-primeroUltimo = obtenerPrimerUltimo(listadoPokemones['results'])
+  pag = input("Desea cambiar a la seguiente pagina a/s: ")
+  while True:
+    patron2 = "^[as]{1}$"
+    resultado2 = re.search(patron2, pag)
+    if resultado2:
+        break
+    pag = input("Desea cambiar a la seguiente pagina a/s: ")
+    
+  if pag == "s":
+    x2 = x2 + limite
+  else:
+    x2 = x2 - limite
 
-print(primeroUltimo)
 
-primero = primeroUltimo[0]
-ultimo = primeroUltimo[1]
-
-nom_digitado = buscarPokemonPorNombre(REGEX_NOMBRES)
-id_digitado = buscarPokemonPorId(primero,ultimo,REGEX_NUMBERS)
-if nom_digitado:
-  datos = verPokemonID(BASE_URL,id_digitado)
-else:
-  datos1 = verPokemonnom(BASE_URL,nom_digitado)
-habilidadesPoke(datos)
-tiposPoke(datos)
 
 
     
